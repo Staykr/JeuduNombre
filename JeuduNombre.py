a9b76400d3eb79586c12ff5e351030b0c3c623e9
Staykr <147034784+Staykr@users.noreply.github.com>
2023-11-22 12:44:22 +0100

    Update JeuduNombre.py

--- JeuduNombre.py
+++ JeuduNombre.py
@@ -1,8 +1,7 @@
 import random
 
 def ask_int(message: str) -> int:
-     # Vérifier si l'entrée peut être convertie en entier
-    while True :
+    while True:
         try:
             proposition: int = int(input(message))
             return proposition
@@ -14,42 +13,38 @@ def ask_int_in_range(message: str, min: int, max: int) -> int:
         value: int = ask_int(message)
         if value >= min and value <= max:
             return value
-    
-        print("Veuillez entrer un nombre entre " + str(min) + " et " + str(max))
+        print("Veuillez entrer un nombre entre", min, " et ", max)
 
-# Demander à l'utilisateur de définir les bornes    
 borne_min = ask_int("Entrez la borne minimale : ")
 borne_max = ask_int("Entrez la borne maximale : ")
 
-def JeuduNombre():
+def JeuDuNombre():
     # Générer un nombre aléatoire entre 2 bornes
-    print("Devinez le nombre secret entre",borne_min,"et",borne_max)
+    print("Devinez le nombre secret entre", borne_min," et ", borne_max)
     print("Vous avez 10 essais !!")
 
-    nombre_secret = random.randint(borne_min,borne_max)
+    while True:
+        nombre_secret = random.randint(borne_min, borne_max)
+        essaie = 10
 
-    essaie = 10
-    while essaie != 0:
-        # Obtenir la tentative de l'utilisateur
-        essaie -= 1
+        while essaie != 0:
+            essaie -= 1
+            proposition: int = ask_int_in_range("Devinez le nombre: ", borne_min, borne_max)
 
-        proposition: int = ask_int_in_range("Devinez le nombre: ", borne_min, borne_max)
+            if proposition < nombre_secret:
+                print("Le nombre est plus grand. Essayez à nouveau.")
+            elif proposition > nombre_secret:
+                print("Le nombre est plus petit. Essayez à nouveau.")
+            else:
+                print("Félicitations! Vous avez trouvé le nombre secret en",10 ,essaie,"tentatives.")
+                break
 
-        # Comparer la proposition avec le nombre secret
-        if proposition < nombre_secret:
-            print("Le nombre est plus grand. Essayez à nouveau.")
-        elif proposition > nombre_secret:
-            print("Le nombre est plus petit. Essayez à nouveau.")
-        else:
-           print("Félicitations! Vous avez trouvé le nombre secret en", 10 - essaie, "tentatives.")
-           break           
-    if essaie == 0: 
-        print("Vous n'avez plus d'essais... LOOOOOOSEEEEER ! ")
-    
-    rejoue = int(input("voulez vous rejouez, si oui mettez 1, si non mettez 0 :"))
-    if rejoue == 1:
-        print("Vous allez recommencez le jeu")
-    else:
-        print("Merci de vôtre participation")
-    #Il faut par contre relancer le programme pour le refaire...
-JeuduNombre()
+        if essaie == 0:
+            print("Vous n'avez plus d'essais... LOOOOOOSEEEEER ! ")
+
+        replay = input("Voulez-vous rejouer ? (Oui(yes)/Non(no)): ").lower()
+        if replay != "yes":
+            print("Merci de votre participation.")
+            break
+
+JeuDuNombre()
